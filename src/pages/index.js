03d420b9ebf33efd78cadb48379fd3e815c23844
Colin <1580440944@qq.com>
2021-01-08 16:50:40 +0800

    change index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,74 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+function func(node) {
+  if (node.has_image) {
+    return <img src={node.image.src} class="card-img-top" alt="..." />
+  } else {
+    return ""
+  }
+}
+export default ({ data }) => {
+  //   console.log(data)
+  return (
+    <Layout>
+      <div>
+        <h1>My readings</h1>
+        <container>
+          {data.allPocketArticle.edges.map(({ node }, index) => (
+            <div class="card mb-3" key={index}>
+              <div class="card-body">
+                <h3 class="card-title">
+                  <Link to={node.url} color="teal">
+                    {node.title}
+                  </Link>
+                </h3>
+                {func(node)}
+                <p class="card-text">{node.excerpt}</p>
+                <p class="card-text">
+                  <small class="text-muted">
+                    Total {node.word_count} words from{" "}
+                    <u>{node.articleDomain}</u>
+                  </small>
+                </p>
+              </div>
+            </div>
+          ))}
+        </container>
+      </div>
+    </Layout>
+  )
+}
 
-export default IndexPage
+export const query = graphql`
+  query PageQuery {
+    allPocketArticle(sort: { fields: readDay }) {
+      edges {
+        node {
+          id
+          url
+          title
+          favourite
+          excerpt
+          is_article
+          is_index
+          has_video
+          has_image
+          word_count
+          time_read
+          readDay
+          readWeek
+          articleDomain
+          domainFavicon
+          image {
+            item_id
+            src
+            width
+            height
+          }
+        }
+      }
+    }
+  }
+`
